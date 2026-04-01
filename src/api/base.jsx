ad87b6d7aeef051d9ea8a2e381fa468a7f6381f5
Attaullah <attaullah@gmail.com>
2026-04-01 16:47:16 +0500

    updating the base file

--- src/api/base.jsx
+++ src/api/base.jsx
@@ -4,7 +4,7 @@ export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
     baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1`, // your backend URL
-    credentials: 'include', // IMPORTANT for session-based auth
+    credentials: 'include',  // IMPORTANT for session-based auth
   }),
   tagTypes: [
     'Admin',
