f48639c40d132f25a0ae403dab878538acd7001d
Attaullah <attaullah@gmail.com>
2026-04-08 12:25:57 +0500

    remove the input

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,8 +3,8 @@ import { Mail, Lock, Eye, EyeOff, ArrowRight, Shield } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useAdminLoginMutation } from "../../api/auth";
 const Login = () => {
-  const [email, setEmail] = useState("admin@healthbridge.com");
-  const [password, setPassword] = useState("admin123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState("");
   
